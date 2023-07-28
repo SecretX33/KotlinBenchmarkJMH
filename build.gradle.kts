16a539c54e459506928eddcd402c2afadad47888
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2023-07-28 16:09:49 -0300

    Update dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,16 @@ import org.jetbrains.kotlin.com.google.common.reflect.ClassPath
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
-    kotlin("kapt") version "1.8.10"
-    kotlin("plugin.allopen") version "1.8.10"
+    kotlin("jvm") version "1.9.0"
+    kotlin("kapt") version "1.9.0"
+    kotlin("plugin.allopen") version "1.9.0"
     id("me.champeau.jmh") version "0.6.8"
     id("io.morethan.jmhreport") version "0.9.0"
     application
 }
 
 group = "com.github.secretx33.codebench"
-version = "1.0-SNAPSHOT"
+version = "0.1"
 
 repositories {
     mavenCentral()
